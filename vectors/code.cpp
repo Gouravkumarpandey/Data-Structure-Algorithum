abccc380f37de81e0f2ba72d1c80f485e2d1dc3f
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2024-12-28 16:18:20 +0530

    added

--- vectors/code.cpp
+++ vectors/code.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 int main (){
-    vector <char> vec ={ 'a','b','c','d','e','f'};
-cout<< "size = " << vec.size() << endl;
-cout<< "first = " <<vec.front() <<endl;
-cout<< "last =" << vec.back() << endl;
-cout<< "at 2 = "<< vec.at(3) << endl;
-cout<< "empty =" << vec.empty()<< endl;
-vec.push_back('g');
-vec.pop_back( );
+    vector <int> vec ={ };
+
+vec.push_back(10);
+vec.push_back(28);
+vec.push_back(31);
+cout<< vec<< endl;
+cout<< "size" <<vec.size() <<endl;
     for(char val : vec){
         cout<< val << endl;
 
